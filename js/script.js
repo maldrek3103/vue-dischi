24228252b5f073b17a3e65d4405e01853c3cd9bc
maldrek3103 <cpisanu96@gmail.com>
2021-09-12 16:36:29 +0200

    Added vue script

--- js/script.js
+++ js/script.js
@@ -4,6 +4,26 @@ Vue.config.devtools = true;
 
 const app = new Vue({
   el: '#app',
-  data: {},
+  data: {
+    albums: [],
+  },
   methods: {},
+  created() {
+    axios.get('https://flynn.boolean.careers/exercises/api/array/music').then((res) => {
+      const response = res.data.response;
+      this.albums = response;
+    })
+  }
 });
+
+
+
+
+
+// {
+//   "poster": "https://www.onstageweb.com/wp-content/uploads/2018/09/bon-jovi-new-jersey.jpg",
+//   "title": "New Jersey",
+//   "author": "Bon Jovi",
+//   "genre": "Rock",
+//   "year": "1988"
+// },
